39a5b1f6ff834c57fd62501cbd57bd5059fd36bf
nikitaj-57 <nikitajit2@gmail.com>
2021-12-02 07:34:31 +0530

    added new code files to recursion using arrays

---
+++ Recursion with Arrays/3.maxofArray.java
@@ -0,0 +1,30 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        // write your code here
+        Scanner scn = new Scanner(System.in);
+        int n = scn.nextInt();
+        int[] arr = new int[n];
+        for(int i=0;i<arr.length;i++) {
+            arr[i]=scn.nextInt();
+        }
+        System.out.println(maxOfArray(arr,0));
+        
+    }
+
+    public static int maxOfArray(int[] arr, int idx){
+        if(idx==arr.length-1) {
+            return arr[idx];
+        }
+        int maxtemp = maxOfArray(arr,idx+1);
+        if(maxtemp<arr[idx]) {
+            return arr[idx];
+        } else {
+            return maxtemp;
+        }
+    }
+
+}
---
+++ Recursion with Arrays/5.lastIndex.java
@@ -0,0 +1,31 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        // write your code here
+        Scanner scn = new Scanner(System.in);
+        int n = scn.nextInt();
+        int[] arr = new int[n];
+        for(int i=0;i<arr.length;i++) {
+            arr[i]=scn.nextInt();
+        }
+        int x = scn.nextInt();
+
+        System.out.println(lastIndex(arr,arr.length-1,x));
+    }
+
+    public static int lastIndex(int[] arr, int idx, int x){
+        if(idx == -1) {
+            return -1;
+        }
+        int temp = lastIndex(arr,idx-1,x);
+        if(arr[idx]==x) {
+            return idx;
+        } else {
+            return temp;
+        }
+    }
+
+}
